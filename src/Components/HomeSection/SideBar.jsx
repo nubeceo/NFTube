91f43b0f688c42e5766787d07a4a955e2ff08561
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2024-01-20 21:27:56 +0530

    updated

--- src/Components/HomeSection/SideBar.jsx
+++ src/Components/HomeSection/SideBar.jsx
@@ -1,23 +1,101 @@
-import React, { createElement, useState, useContext } from 'react'
+import React, { createElement, useState, useEffect, useContext } from 'react'
 import { motion } from 'framer-motion'
 import { Link } from 'react-router-dom'
-import { FaHome, FaUserAstronaut, FaHistory } from "react-icons/fa";
+import { FaHome, FaUserAstronaut, FaHistory, FaArrowDown, FaAngleDown } from "react-icons/fa";
 import { RiWechatChannelsLine } from "react-icons/ri";
 import { TbCategory } from "react-icons/tb";
 import UserContext from '../../Context/UserContext';
+import axios from 'axios';
+
+
 
 function SideBar() {
+    //  importing open from usercontext
+    const { SetOpen, Open } = useContext(UserContext);
 
+
+    // menu array
     const menu = [
         { name: "Home", link: '/', icon: FaHome },
         { name: "User", link: '/', icon: FaUserAstronaut },
         { name: "History", link: '/', icon: FaHistory },
-        { name: "Top Channels", link: '/', icon: RiWechatChannelsLine },
+        {
+            name: "Top Channels", link: '/', icon: RiWechatChannelsLine, subRoutes: [
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZTM49hEx9zyui_0zDZKi75EgqJPJfuDmWu1cePdQQ=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "MrBeast",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/5oUY3tashyxfqsjO5SGhjT4dus8FkN9CsAHwXWISFrdPYii1FudD4ICtLfuCw6-THJsJbgoY=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "PewDiePie ",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZSS262qsZXYQwuo2ajP-JCp_5XSPJ9_NorTenliGg=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "Total Gaming",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZRGX_4_QzfvKP6XnYRS8fhK5r0drPcvUBa_EMHxfA=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "Mo Vlogs",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZTJa98kcY4SsISd-ibezrzLEIDXc-iaTn4QC1gYfQ=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "imKay",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/j01juFvKwHnKHdgcklpPKLkfNBuGbGJKLBwXVhbN_5LeCU3S9bTsHBL-MKPRQCjpZpfPJ_dJ=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "CarryMinati",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZQ2v2988cr0ftN-GZRvr6D7vj_3E_z_pCnAScuhjg=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "Mumbiker Nikhil",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZTNiWLD2-Qw1eWFmRJkGBzfI-8oFNmYk5tBnpNurQ=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "CodeWithHarry",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/nhDLqeIgXMJBDIrX2bXavvHoWX0tsslDEh7k2xZ1P0W8b_CMRVigp2kxJubYEVwBcBlogZDe=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "Apna College",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZTDkajQxPa4sjDOW-c3er1szXkSAO-H9TiF4-8u_Q=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "freeCodeCamp",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/wg1TITEoPfxvBGfzuqWyt3bqm_qu35ZhMswUv3feetU3xNX_6wsAXZF40OlPIgY4TmqbqCmAZ1U=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "JavaScript Mastery",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/ytc/AIf8zZR7ZLXsol2-5X-UnXXZfFqN3rehkDpHPBkB6gZwzg=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "george hotz archive",
+                },
+                {
+                    pic: "https://yt3.googleusercontent.com/9uXIJnlvXJmCzJ4Buh_9FjRYhiqM7vK1dD80kTTv12iXapl1NoIspJddwoXihGvXOnIijgaLy40=s176-c-k-c0x00ffffff-no-rj",
+
+                    name: "Coding Jesus",
+                },
+
+
+            ]
+        },
         { name: "Explore", link: '/', icon: TbCategory },
 
     ];
 
-    const { SetOpen, Open } = useContext(UserContext);
+
+
 
 
     return (
@@ -38,26 +116,68 @@ function SideBar() {
             </div> */}
 
 
+
+            {/* mapping menus */}
             {
-                menu?.map((menu, i) => (
-                    <Link to={menu.link} key={i} className={` group  flex items-center text-md gap-3.5 font-medium py-2 px-2 pl-3 my-3  hover:bg-gray-900 rounded-md`}>
+                menu?.map((menu, i) => {
 
-                        <div>{React.createElement(menu.icon, { size: "20" })}</div>
 
-                        <h2 
-                        style={{
-                            transitionDelay:`${i+3}00ms`
-                        }}
-                        className={`whitespace-pre duration-500 ${!Open && 'opacity-0 translate-x-28 overflow-hidden'} `}>{menu.name}
+                    if (menu.subRoutes) {
+                        return <div className=' group cursor-pointer flex items-center text-md gap-3 font-medium py-2 px-2 pl-3 my-3  hover:bg-gray-900 rounded-md '>
 
-                        </h2>
 
-                        <h2 className={` ${Open && "hidden"}   absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-20 group-hover:duration-300 group-hover:w-fit`}>
-                            {menu.name}
-                        </h2>
-                    </Link>
-                ))
+                            <div>{React.createElement(menu.icon, { size: "20" })}</div>
+
+                            <h2
+                                style={{
+                                    transitionDelay: `${i + 3}00ms`
+                                }}
+                                className={`whitespace-pre duration-500 ${!Open && 'opacity-0 translate-x-28 overflow-hidden '} `}>{menu.name}
+
+                            </h2>
+
+                            <div style={{
+                                transitionDelay: `${i + 3}00ms`
+                            }}
+                                className={`whitespace-pre duration-500 ${!Open && 'opacity-0 translate-x-28 overflow-hidden '} `}>
+                                <FaAngleDown />
+                            </div>
+
+
+
+                            <h2 className={` ${Open && "hidden"}   absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-20 group-hover:duration-100 group-hover:w-fit`}>
+                                {menu.name}
+                            </h2>
+
+
+                        </div>
+
+                    }
+
+                    return (
+                        <Link to={menu.link} key={i} className={` group  flex items-center text-md gap-3.5 font-medium py-2 px-2 pl-3 my-3  hover:bg-gray-900 rounded-md`}>
+
+                            <div>{React.createElement(menu.icon, { size: "20" })}</div>
+
+                            <h2
+                                style={{
+                                    transitionDelay: `${i + 3}00ms`
+                                }}
+                                className={`whitespace-pre duration-500 ${!Open && 'opacity-0 translate-x-28 overflow-hidden '} `}>{menu.name}
+
+                            </h2>
+
+                            <h2 className={` ${Open && "hidden"}   absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-20 group-hover:duration-300 group-hover:w-fit`}>
+                                {menu.name}
+                            </h2>
+                        </Link>
+                    )
+                })
             }
+
+
+
+
 
         </div>
     )
---
+++ src/Components/HomeSection/SideBarsub.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SideBarsub() {
+  return (
+    <div>SideBarsub</div>
+  )
+}
+
+export default SideBarsub
