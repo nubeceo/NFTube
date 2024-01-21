007bd5ddc7898c768d3e444e093c94d23a8b7f4a
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2024-01-21 17:28:12 +0530

    updated

--- src/Components/HomeSection/Home.jsx
+++ src/Components/HomeSection/Home.jsx
@@ -6,6 +6,7 @@ import { FetchFromApi } from '../../Utils/FtechFromApi';
 
 
 function Home() {
+  
   const { SetOpen, Open } = useContext(UserContext);
 
 
@@ -16,78 +17,66 @@ function Home() {
   // url for fetching feed datat from yt datat api
   // const feedUrl ="https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchQuery}&type=video&key=${youtubeApiKey}";
 
-
-
-
   const [selectedCategory, setselectedCategory] = useState('New');
   const [videos, setvideos] = useState([]);
 
   // fetching feed on screen load
-  
-
-    // // 
-
-    // FetchFromApi(`search?part=snippet&maxResults=50&q=${selectedCategory}&key=AIzaSyA3VbXXid8BNWtXtLXbTXjXsipnYbwSpW4`)
-    // .then((data)=>setvideos(data))
 
 
+  // importing api
+  const API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_API_KEY;
 
 
-
-    // https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${selectedCategory}&key=AIzaSyA3VbXXid8BNWtXtLXbTXjXsipnYbwSpW4
-
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${selectedCategory}&key=AIzaSyA3VbXXid8BNWtXtLXbTXjXsipnYbwSpW4`);
-          if (!response.ok) {
-            throw new Error('Network request failed');
-          }
-          const result = await response.json();
-          setvideos(result.items);
-        } catch (error) {
-          setError(error);
-        } finally {
-          setIsLoading(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=${selectedCategory}&key=${API_KEY}`);
+        if (!response.ok) {
+          throw new Error('Network request failed');
         }
-      };
+        const result = await response.json();
+        setvideos(result.items);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-      fetchData();
-    }, []);
-
-
-
-
-
-    return (
-      <div className='w-full flex flex-col top-11 md:flex-row  lg:flex-row items-start justify-end'>
-
-
-        {/* side bar section */}
-        <div className={` w-full lg:w-1/6  ${Open ? 'lg:w-1/6' : 'lg:w-24'} duration-500   sm:top-11 left-0 fixed h-20 lg:h-screen items-start`}>
-
-          {/* sidebar component */}
-          <SideBar
-            selectedCategory={selectedCategory}
-            setselectedCategory={setselectedCategory}
-          />
-
-        </div>
+    fetchData();
+  }, []);
 
 
 
 
-        {/* feed section */}
-        <div className={`  right-0 top-11  h-full w-full lg:w-5/6 ${Open ? 'lg:w-5/6' : 'lg:w-custom'}  duration-500 p-24 lg:p-6`}>
 
-          {/* video card component */}
-          <Videos selectedCategory={selectedCategory} videos={videos} />
+  return (
+    <div className='w-full flex flex-col top-11 md:flex-row  lg:flex-row items-start justify-end'>
 
-        </div>
 
+      {/* side bar section */}
+      <div className={` w-full lg:w-1/6  ${Open ? 'lg:w-1/6' : 'lg:w-24'} duration-500   sm:top-11 left-0 fixed h-20 lg:h-screen items-start`}>
+
+        {/* sidebar component */}
+        <SideBar
+          selectedCategory={selectedCategory}
+          setselectedCategory={setselectedCategory}
+        />
 
       </div>
-    )
-  }
+
+
+
+
+      {/* feed section */}
+      <div className={`  right-0 top-11  h-full w-full lg:w-5/6 ${Open ? 'lg:w-5/6' : 'lg:w-custom'}  duration-500 p-24 lg:p-6`}>
+
+        {/* video card component */}
+        <Videos selectedCategory={selectedCategory} videos={videos} />
+
+      </div>
+
+
+    </div>
+  )
+}
 
 export default Home;
--- src/Utils/FtechFromApi.js
+++ src/Utils/FtechFromApi.js
@@ -1,26 +1,23 @@
-import axios from "axios";
+// import axios from "axios";
 
 
-// https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=dog&key=AIzaSyA3VbXXid8BNWtXtLXbTXjXsipnYbwSpW4
+// const BASE_URL = 'https://youtube.googleapis.com/youtube/v3/';
 
-const BASE_URL = 'https://youtube.googleapis.com/youtube/v3/';
 
-const YOUTUBE_API_KEY = import.meta.env.VITE_REACT_APP_YOUTUBE_API_KEY;
-
-const options = {
-    // url: BASE_URL,
-    params: {
-        // part: 'snippet',
-        // videoId: 'M7FIvfx5J10'
-        key: YOUTUBE_API_KEY,
-    },
-    // headers: {
-    //     key: YOUTUBE_API_KEY,
-    //     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    // }
+// const options = {
+//     url: BASE_URL,
+//     params: {
+//         part: 'snippet',
+//         videoId: 'M7FIvfx5J10'
+//         key: YOUTUBE_API_KEY,
+//     },
+//     headers: {
+//         key: YOUTUBE_API_KEY,
+//         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+//     }
 
     
-};
+// };
 
 // try {
 //     const response = await axios.request(options);
@@ -29,18 +26,18 @@ const options = {
 //     console.error(error);
 // }
 
-export const FetchFromApi = async (url) => { 
-    // const {data} = await axios.get(`${BASE_URL}/${url}`)
-    const response = await fetch(`${BASE_URL}/${url}`);
+// export const FetchFromApi = async (url) => { 
+//     // const {data} = await axios.get(`${BASE_URL}/${url}`)
+//     const response = await fetch(`${BASE_URL}/${url}`);
 
-    if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-    }
+//     if (!response.ok) {
+//         throw new Error(`Error: ${response.statusText}`);
+//     }
 
-    const data = await response.json();
+//     const data = await response.json();
 
-    return data;
-}
+//     return data;
+// }
 
 
 
